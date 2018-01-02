5fd0d77f78c79833bdb1cda2e5c388acede8c92d
Lucas Ferreira <lucas.santos@fcamara.com.br>
2018-01-02 15:36:10 -0200

    death commit

--- src/app/code/Lucas/CRUD/Model/ResourceModel/LucasCRUD/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Lucas\Model\ResourceModel\LucasCRUD;
-use \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
-
-class Collection extends AbstractCollection{
-    public function _construct(){
-        $this->_init('Lucas\CRUD\Model\LucasCRUD','Lucas\CRUD\Model\ResourceModel\LucasCRUD');
-    }
-}
